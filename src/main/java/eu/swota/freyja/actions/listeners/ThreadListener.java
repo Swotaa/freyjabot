23099da10d282352f030c8c85b4e3a9ee3e5f06e
Alban Richard <albanrichard48@gmail.com>
2026-02-26 14:34:38 +0100

    Update ThreadListener.java
    added listening to onChannelCreate event

--- src/main/java/eu/swota/freyja/actions/listeners/ThreadListener.java
+++ src/main/java/eu/swota/freyja/actions/listeners/ThreadListener.java
@@ -3,8 +3,12 @@ package eu.swota.freyja.actions.listeners;
 import eu.swota.freyja.database.DatabaseManager;
 import eu.swota.freyja.sheets.SheetConfig;
 import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
+import net.dv8tion.jda.api.events.channel.ChannelCreateEvent;
 import net.dv8tion.jda.api.events.channel.ChannelDeleteEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.LocalDateTime;
 
 public class ThreadListener extends ListenerAdapter {
 
@@ -14,7 +18,7 @@ public class ThreadListener extends ListenerAdapter {
     }
 
     @Override
-    public void onChannelDelete(ChannelDeleteEvent event) {
+    public void onChannelDelete(@NotNull ChannelDeleteEvent event) {
         long forumId = SheetConfig.get().issueBoardId;
 
         if (!(event.getChannel() instanceof ThreadChannel thread)) {
@@ -29,5 +33,23 @@ public class ThreadListener extends ListenerAdapter {
 
         System.out.println("Thread supprimé : " + deletedThreadId);
         db.deleteIssue(event.getGuild().getId(), thread.getId());
+    }
+
+    @Override
+    public void onChannelCreate(@NotNull ChannelCreateEvent event) {
+        long forumId = SheetConfig.get().issueBoardId;
+
+        if (!(event.getChannel() instanceof ThreadChannel thread)) {
+            return;
+        }
+
+        if (thread.getParentChannel().getIdLong() != forumId) {
+            return;
+        }
+
+        long addedThreadId = thread.getIdLong();
+
+        System.out.println("Thread ajouté : " + addedThreadId);
+        db.addIssue(event.getGuild().getId(), thread.getId(), LocalDateTime.now().toString());
     }
 }
